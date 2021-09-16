d96f53ca975e4ed406509e949249447b18514f48
amha19 <34722096+amha19@users.noreply.github.com>
2021-09-16 13:09:04 -0500

    configure eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -4,48 +4,48 @@ import Cons from './components/Cons';
 import './App.css';
 
 export type ItemList = {
-  item: string;
-  isPro: boolean;
-}
+    item: string;
+    isPro: boolean;
+};
 
 function App() {
-  const [text, setText] = useState("");
-  const [list, setList] = useState<ItemList[]>([]);
+    const [text, setText] = useState('');
+    const [list, setList] = useState<ItemList[]>([]);
 
-  const handleBtns = (item: string) => {
-    if (text) {
-      if (item === "pro") setList([...list, { item: text, isPro: true }]);
-      if (item === "con") setList([...list, { item: text, isPro: false }]);
-    }
-    setText("");
-  }
+    const handleBtns = (item: string) => {
+        if (text) {
+            if (item === 'pro') setList([...list, { item: text, isPro: true }]);
+            if (item === 'con') setList([...list, { item: text, isPro: false }]);
+        }
+        setText('');
+    };
 
-  return (
-    <div className="App">
-      <h1>Don't be in a haste</h1>
-      <div>
-        <button
-          onClick={() => {
-            handleBtns("pro");
-          }}
-        >
-          Left
-        </button>
-        <input onChange={(event) => setText(event.target.value)} value={text} />
-        <button
-          onClick={() => {
-            handleBtns("con");
-          }}
-        >
-          Right
-        </button>
-      </div>
-      <div className="list-container">
-        <Pros prosList={list} />
-        <Cons consList={list} />
-      </div>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Don't be in a haste</h1>
+            <div>
+                <button
+                    onClick={() => {
+                        handleBtns('pro');
+                    }}
+                >
+                    Left
+                </button>
+                <input onChange={(event) => setText(event.target.value)} value={text} />
+                <button
+                    onClick={() => {
+                        handleBtns('con');
+                    }}
+                >
+                    Right
+                </button>
+            </div>
+            <div className="list-container">
+                <Pros prosList={list} />
+                <Cons consList={list} />
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/components/Pros.tsx
+++ src/components/Pros.tsx
@@ -1,25 +1,25 @@
-import { useEffect, useState } from "react";
-import { ItemList } from "../App";
+import { useEffect, useState } from 'react';
+import { ItemList } from '../App';
 
 const Pros = (props: { prosList: ItemList[] }) => {
-  const { prosList } = props;
+    const { prosList } = props;
 
-  const [pros, setPros] = useState<ItemList[]>([]);
+    const [pros, setPros] = useState<ItemList[]>([]);
 
-  useEffect(() => {
-    setPros(prosList.filter((pro) => pro.isPro));
-  }, [prosList]);
+    useEffect(() => {
+        setPros(prosList.filter((pro) => pro.isPro));
+    }, [prosList]);
 
-  return (
-    <div>
-      <h3>Pros List</h3>
-      <ul>
-        {pros.map((pro, index) => (
-          <li key={index}>{pro.item}</li>
-        ))}
-      </ul>
-    </div>
-  );
+    return (
+        <div>
+            <h3>Pros List</h3>
+            <ul>
+                {pros.map((pro, index) => (
+                    <li key={index}>{pro.item}</li>
+                ))}
+            </ul>
+        </div>
+    );
 };
 
 export default Pros;
